b5547f994cf78e06d0c0b1df2f4d422f0d857b80
gianpaolo papaiz <gianpaolopapaiz@hotmail.com>
2021-06-08 12:53:38 -0300

    started product controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,30 @@
 class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+  end
+  def show
+    @product = Product.find(params[:id])
+  end
+  def new
+    @product = Product.new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+  def create
+    @product = Product.new(product_params)
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @product.restaurant = @restaurant
+    if @product.save
+      flash[:success] = "Product successfully created"
+      redirect_to restaurant_products_path(@restaurant)
+    else
+      flash[:error] = "Something went wrong"
+      render :new
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:products).permit(:name, :description, :price, :category, :photo)
+  end
 end
