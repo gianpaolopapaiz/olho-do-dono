4ff9b1955e7a0d17f10dbaa959a9f6480f593d4d
Marcio Silva <mas16_06@hotmail.com>
2021-06-08 12:09:19 -0300

    nothing

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,7 +16,6 @@ require("channels")
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-
 // ----------------------------------------------------
 // Note(lewagon): ABOVE IS RAILS DEFAULT CONFIGURATION
 // WRITE YOUR OWN JS STARTING FROM HERE 👇
@@ -25,10 +24,13 @@ require("channels")
 // External imports
 import "bootstrap";
 
+
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+
+
 });
