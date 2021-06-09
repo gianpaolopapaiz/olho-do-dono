c2bfa03b19f1156bd6ae710ab708f5352413aebe
gianpaolo papaiz <gianpaolopapaiz@hotmail.com>
2021-06-09 15:40:06 -0300

    added table logic

--- app/controllers/tables_controller.rb
+++ app/controllers/tables_controller.rb
@@ -1,2 +1,56 @@
 class TablesController < ApplicationController
+
+  def show
+    @table = Table.find(params[:id])
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    if @restaurant.user != current_user
+      redirect_to restaurants_path
+    end
+  end
+
+  def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    if @restaurant.user != current_user
+      redirect_to restaurants_path
+    end
+    @number = params[:number]
+  end
+
+  def create
+    restaurant = Restaurant.find(params[:restaurant_id])
+    if restaurant.user != current_user
+      redirect_to restaurants_path
+    end
+    @number = params[:number]
+    table = Table.new
+    table.restaurant = restaurant
+    table.number = @number
+    table.status = 'open'
+    @message = ''
+    if restaurant.tables.where("number = #{@number} AND status = 'open'").length > 0
+      @message = 'Table has open session already!'
+    else
+      if table.save
+        redirect_to "/restaurants/#{restaurant.id}/spaces/#{@number}/tables/#{table.id}"
+      else
+        @message = 'Opening table error!'
+      end
+    end
+  end
+
+  def close
+    restaurant = Restaurant.find(params[:restaurant_id])
+    if restaurant.user != current_user
+      redirect_to restaurants_path
+    end
+    table = Table.find(params[:id])
+    number = table.number
+    table.status = 'closed'
+    if table.save
+      redirect_to "/restaurants/#{restaurant.id}/spaces/#{number}/tables/new"
+    else
+      render :show
+    end
+  end
+
 end
--- app/models/table.rb
+++ app/models/table.rb
@@ -2,7 +2,7 @@ class Table < ApplicationRecord
   belongs_to :restaurant
   has_many :order_items
   has_many :products, through: :order_items
-  validates :number, :status, :payment_types, presence: true
-  validates :status, inclusion: { in: %w[available occupied closed]}
+  validates :number, :status, presence: true
+  validates :status, inclusion: { in: %w[available open closed]}
 
 end
