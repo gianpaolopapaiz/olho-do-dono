c7e06e961bee302d6fa21aebe2fa29a367b6a9a8
Marcio Silva <mas16_06@hotmail.com>
2021-06-10 16:06:57 -0300

    Some fields of Restaurant show (in green) already implememted in Restaurant Controller

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -6,6 +6,35 @@ class RestaurantsController < ApplicationController
 
   def show
     @restaurant = Restaurant.find(params[:id])
+    items = @restaurant.order_items.where(status: 'paid')
+    today_year = Date.today.year
+    today_month = Date.today.month
+    today_day = Date.today.day
+    @year_amount = 0
+    @year_order_qty = 0
+    @month_amount = 0
+    @month_order_qty = 0
+    @day_amount = 0
+    @day_order_qty = 0
+    items.each do |item|
+      if item.updated_at.year == today_year
+        @year_amount += item.product_quantity * item.product.price
+        @year_order_qty += 1
+        if item.updated_at.month == today_month
+          @month_amount += item.product_quantity * item.product.price
+          @month_order_qty += 1
+          if item.updated_at.day == today_day
+            @day_amount += item.product_quantity * item.product.price
+            @day_order_qty += 1
+          end  
+        end
+      end
+    end
+    @nbr_foods = @restaurant.products.where(category: 'Food').count
+    @nbr_beverages = @restaurant.products.where(category: 'Beverage').count
+    @nbr_desserts = @restaurant.products.where(category: 'Dessert').count
+    @expense_hash = @restaurant.expenses.where('due_date >?', Date.today).each_with_object(Hash.new(0)) {|expense, hash| hash[expense.category] += expense.amount }
+    
   end
 
   def new
