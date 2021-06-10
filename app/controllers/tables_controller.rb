aa5c96f7b7de616bbde7ebf359bd69d29f0cc8a6
gianpaolo papaiz <gianpaolopapaiz@hotmail.com>
2021-06-10 18:51:19 -0300

    added checkout logic

--- app/controllers/tables_controller.rb
+++ app/controllers/tables_controller.rb
@@ -34,7 +34,7 @@ class TablesController < ApplicationController
     number = params[:number]
     table = Table.new
     table.restaurant = restaurant
-    table.number = @number
+    table.number = number
     table.status = 'open'
     if restaurant.tables.where("number = #{number} AND status = 'open'").length > 0
       flash[:alert] = 'Table already has an open session'
@@ -43,10 +43,18 @@ class TablesController < ApplicationController
       if table.save
         redirect_to "/restaurants/#{restaurant.id}/spaces/#{number}/tables/#{table.id}"
       else
-        flash[:alert] = 'Something went wrong'
+        flash[:alert] = table.errors.messages
         redirect_to "/restaurants/#{restaurant.id}/spaces/#{number}/tables/new"
       end
     end
+  end
+
+  def update
+    table = Table.find(params[:id])
+    if !table.update(table_params)
+      flash[:alert] = table.errors.messages
+    end
+    redirect_to "/restaurants/#{table.restaurant.id}/spaces/#{table.number}/tables/#{table.id}"
   end
 
   def close
@@ -60,9 +68,15 @@ class TablesController < ApplicationController
     if table.save
       redirect_to "/restaurants/#{restaurant.id}/spaces/#{number}/tables/new"
     else
-      flash[:alert] = 'Something went wrong'
+      flash[:alert] = table.errors.messages
       redirect_to "/restaurants/#{restaurant.id}/spaces/#{@number}/tables/#{table.id}"
     end
+  end
+
+  private
+
+  def table_params
+    params.require(:table).permit(:payment_type, :rating, :comment)
   end
 
 end
